a970ce7511efef1f5e953d83be036301fe849259
shlomit <shlomitn80@gmail.com>
2022-08-02 18:12:13 +0300

    VEERSION2

---
+++ server.js
@@ -0,0 +1,9 @@
+const http=require("http");
+const app = require("./app");
+const server = http.createServer(app);
+const port = process.env.PORT || 8787
+server.listen(port,()=>{
+console.log("Server On Air");
+
+
+});
